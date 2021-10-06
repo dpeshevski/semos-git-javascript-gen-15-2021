9bcd3c2bbe92c608d53496e363d354d2883835ca
Daniel <d.pesevski92@gmail.com>
2021-10-06 20:51:58 +0200

    Makes some updates into hello.js and studetns.txt, renamed proba to style.css

--- hello.js
+++ hello.js
@@ -1,4 +1,10 @@
-function sayHi() {
-  console.log('Hello World');
-  //pecati 'Hello World' vo console
+// variable message
+const message = 'Hello World';
+
+function sayHi(message) {
+  // return message parameter
+  return message;
 }
+
+// call function sayHi and add a message as param
+console.log(sayHi(message));
